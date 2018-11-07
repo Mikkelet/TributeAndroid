2b9604551cc0efb0b4b9ec7c872f1819c85124b9
Mikkelet <Mikkelet@gmail.com>
2018-11-07 16:05:51 +0100

    +1

--- app/src/main/java/com/tributedummy/metbb/dummy3/MainActivity.java
+++ app/src/main/java/com/tributedummy/metbb/dummy3/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(falseg);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         bottomNavigationView = findViewById(R.id.navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
